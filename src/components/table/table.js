34d6f8106bd84f94fefe4d36f5683dcead6a870c
Inontran <pakim.wesi@gmail.com>
2021-11-18 17:24:37 +0900

    Сделана таблица. Реализована вставка данных в таблицу.

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,1 +1,64 @@
 import './table.scss';
+
+class Table {
+  _tableContainer;
+  _tableBody;
+
+  constructor(tableContainer) {
+    this._tableContainer = tableContainer;
+    this._init();
+  }
+
+  fillTable(dataRating) {
+    if (!this._tableBody) {
+      this._tableBody = document.createElement('tbody');
+      this._tableBody.classList.add('table__body');
+      this._tableContainer.append(this._tableBody);
+    }
+
+    const friendIds = [];
+    dataRating.friends.forEach(friend => {
+      friendIds.push(friend.id);
+    });
+
+    const sortedUsersArray = dataRating.rating.sort(function(user1, user2){
+      const user1Points = parseInt(user1.points, 10);
+      const user2Points = parseInt(user2.points, 10);
+      if (user2Points > user1Points) return 1;
+      if (user2Points == user1Points) return 0;
+      if (user2Points < user1Points) return -1;
+    });
+
+    let tableBodyContent = '';
+    let numberPlace = 1;
+    sortedUsersArray.forEach(player => {
+      let tableUsernameModifier = 'table__username';
+      if (friendIds.indexOf(player.id) !== -1) {
+        tableUsernameModifier += ' table__username_friend';
+      }
+
+      let strNewRow = `<tr class="table__body-row">
+                        <td class="table__body-cell table__body-cell_with-place">${numberPlace}</td>
+                        <td class="table__body-cell table__body-cell_with-avatar">
+                          <div class="table__avatar-wrapper"></div>
+                        </td>
+                        <td class="table__body-cell table__body-cell_with-name">
+                          <div class="${tableUsernameModifier}" title="${player.name} ${player.lastName}">
+                            ${player.name} ${player.lastName}
+                          </div>
+                        </td>
+                        <td class="table__body-cell table__body-cell_with-experience">${player.points}</td>
+                      </tr>`;
+      tableBodyContent += strNewRow;
+      numberPlace += 1;
+    });
+
+    this._tableBody.innerHTML = tableBodyContent;
+  }
+
+  _init() {
+    this._tableBody = this._tableContainer.querySelector('.table__body');
+  }
+}
+
+export default Table
--- src/page/entry.js
+++ src/page/entry.js
@@ -2,23 +2,26 @@ import '@/components/button/button';
 import '@/components/map/map';
 import Carousel from '@/components/carousel/carousel';
 import Modal from '@/components/modal/modal';
-import '@/components/table/table';
+import Table from '@/components/table/table';
 
+import data from './data';
 import './style.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
   const carouselContainer = document.querySelector('.js-carousel');
   const carosuel = new Carousel(carouselContainer);
 
-  const modalRating = document.querySelector('#rating-modal');
-  const modal = new Modal(modalRating);
+  const modalContainer = document.querySelector('#rating-modal');
+  const modal = new Modal(modalContainer);
 
   function handleBtnOpenRatingPointerdown() {
     modal.show();
   }
 
-  const btnsOpenRating = document.querySelectorAll('.js-main-window__btn-open-rating .js-button');
-  btnsOpenRating.forEach(btn => {
-    btn.addEventListener('pointerdown', handleBtnOpenRatingPointerdown);
-  });
+  const btnOpenRating = document.querySelector('.js-main-window__btn-open-rating .js-button');
+  btnOpenRating.addEventListener('pointerdown', handleBtnOpenRatingPointerdown);
+
+  const tableContainer = document.querySelector('.js-table');
+  const table = new Table(tableContainer);
+        table.fillTable(data);
 });
